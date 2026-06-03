73699af177c655ec4a44172e899dfdfb881c5a5e
akshay <akshay1dogra26@gmail.com>
2026-06-03 16:14:34 +0530

    Add new Java feature

---
+++ src/HelloWorldProject/Akshay.java
@@ -0,0 +1,12 @@
+package HelloWorldProject;
+
+public class Akshay {
+	int id;
+	String s;
+	Akshay(int i, String string){
+		id=id;
+		s=s;
+		System.out.println("calling Akshay");
+	}
+	
+}
---
+++ src/HelloWorldProject/HelloWorldClass.java
@@ -0,0 +1,10 @@
+package HelloWorldProject;
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hi, Hello World");
+		Akshay aks=new Akshay(1,"akshay");
+		
+	}
+}
